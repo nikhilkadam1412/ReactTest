9df99147e2d153647664748487a658fc41e43156
Nikhil Kadam <nikhil.kadam@duretechnologies.com>
2021-12-01 19:44:31 +0530

    project folder added

---
+++ test_ui/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
